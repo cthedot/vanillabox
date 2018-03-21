b1d6b88c8c5b78a32ce4408fe474da9ab1af6589
cthedot <cthedot@gmail.com>
2018-03-21 14:53:25 +0100

    move init script to HTML for easier check how to use it via view source

--- js/script.js
+++
@@ -1,9 +0,0 @@
-; (function () {
-  'use strict';
-
-  document.addEventListener("DOMContentLoaded", function() {
-    var $extrabox = document.getElementById('extrabox')
-
-    window.boxes = vanillabox(document.querySelectorAll('.box'))
-  })
-}())
